0a348fdd7852ad91d9707df4d3fd6bf8ca9bc309
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2025-11-10 21:51:17 -0500

    Create CreateItemRequest.cs

---
+++ SharedModels/CreateItemRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedModels
+{
+    public class CreateItemRequest
+    {
+        public int ListId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public string? Link { get; set; }
+    }
+}

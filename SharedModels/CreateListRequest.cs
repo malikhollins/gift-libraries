befd8255bc401851a50d56632db46eb80a3a91ca
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2025-11-10 21:13:16 -0500

    Add new request

---
+++ SharedModels/CreateListRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedModels
+{
+    public class CreateListRequest
+    {
+        public int HouseId { get; set; }
+        public string? OwnerId { get; set; }
+        public string? Name { get; set; }
+    }
+}

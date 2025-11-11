827a98802538b4d808b6f18e2d59e4f080aea79b
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2025-11-10 23:06:32 -0500

    Adding owner id to request

--- SharedModels/CreateItemRequest.cs
+++ SharedModels/CreateItemRequest.cs
@@ -2,6 +2,7 @@
 {
     public class CreateItemRequest
     {
+        public int OwnerId { get; set; }
         public int ListId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }

edaa31f4737fb638ea353a88b17a1ed49d4dc65a
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2025-11-15 12:32:27 -0500

    adding update item request

---
+++ SharedModels/UpdateItemRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedModels
+{
+    public class UpdateItemRequest
+    {
+        public int UserId { get; set; }
+
+        public int ListId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Link { get; set; }
+
+        public double Price { get; set; }
+    }
+}

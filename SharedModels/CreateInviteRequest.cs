b0caf9534853905c3c93d471bb598163bf424baf
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2026-01-31 10:12:23 -0500

    Adding create invite request

---
+++ SharedModels/CreateInviteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedModels
+{
+    public class CreateInviteRequest
+    {
+        public int UserId { get; set; }
+        public int HouseId { get; set; }
+        public int UserToInvite { get; set; }
+    }
+}

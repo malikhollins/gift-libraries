9c9a11a253e45d34d3c98f8dd72d6fb5039d9481
Malik "Malachi" Hollins <thesouthographic@gmail.com>
2026-01-31 07:55:04 -0500

    Adding update invite request

---
+++ SharedModels/UpdateInviteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedModels
+{
+    public class UpdateInviteRequest
+    {
+        public int HouseId { get; set; }
+        public int UserId { get; set; }
+        public int Status { get; set; }  
+    }
+}
